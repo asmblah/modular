0b1de8a2d4283784486053ca90125f22f0127664
Daniel Phillimore <dan@ovms.co>
2012-07-10 13:46:21 +0100

    Added/fixed some unit tests

--- tests/mocha/js/modular.js
+++ tests/mocha/js/modular.js
@@ -1,5 +1,30 @@
 define(function () {
     suite("Modular", function () {
+        define("classes/Animal", function () {
+            function Animal(species) {
+                this.species = species || null;
+            }
+
+            Animal.prototype.getSpecies = function () {
+                return this.species;
+            };
+
+            return Animal;
+        });
+        define("classes/Human", [
+            "classes/Animal"
+        ], function (
+            Animal
+        ) {
+            function Human() {
+                Animal.call(this, "Human");
+            }
+
+            Human.prototype = Object.create(Animal.prototype);
+
+            return Human;
+        });
+
         test("publishes support for the AMD pattern", function () {
             chai.assert.ok(define.amd);
         });
@@ -7,6 +32,58 @@ define(function () {
         test("publishes special jQuery AMD support", function () {
             chai.assert.deepEqual(define.amd, {
                 jQuery: true
+            });
+        });
+
+        test("paths beginning with './' are resolved relative to current directory", function (done) {
+            require("classes/World", [
+                "./Animal"
+            ], function (
+                Animal
+            ) {
+                chai.assert.isNull(new Animal().getSpecies());
+
+                done();
+            });
+        });
+
+        test("paths beginning with '../' are resolved relative to parent directory", function (done) {
+            require("classes/World", [
+                "../classes/Animal"
+            ], function (
+                Animal
+            ) {
+                chai.assert.isNull(new Animal().getSpecies());
+
+                done();
+            });
+        });
+
+        test("paths beginning with '/' are resolved relative to root", function (done) {
+            define("/util", function () {
+                return {};
+            });
+
+            require("classes/Parser/English", [
+                "/util"
+            ], function (
+                util
+            ) {
+                chai.assert.deepEqual(util, {});
+
+                done();
+            });
+        });
+
+        test("paths not beginning with '.' or '/' are resolved relative to root", function (done) {
+            require("classes/Parser/English", [
+                "classes/Human"
+            ], function (
+                Human
+            ) {
+                chai.assert.equal(new Human().getSpecies(), "Human");
+
+                done();
             });
         });
 
@@ -40,8 +117,22 @@ define(function () {
                     done();
                 });
             });
+        });
 
+        suite("nested require(...)", function () {
+            test("paths are resolved relative to enclosing module", function () {
+                require("test.js", [
+                    "classes/Human"
+                ], function (
+                    Human
+                ) {
+                    chai.assert.equal(new Human().getSpecies(), "Human");
+                });
+            });
+        });
 
+        suite("pathFilter", function () {
+            // TODO: Improve pathFilter so it can remember absolute mappings (so relative mappings work as expected)
         });
     });
 });
